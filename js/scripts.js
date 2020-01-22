56410aa184f8189671f548e79690f4823c1529dc
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-01-21 23:42:41 -0500

    Updated scripts.js
    added Dragonite, Salamence and Rayquaza

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello World!');
+var pokeRepository = [
+  {name: 'Dragonite',
+  type: ['Dragon','Flying'],
+  height: '2.2 m',
+  weight: '210 kg',
+  pokeDexNum: '149'},
 
-var favoriteFood = 'PIZZA!';
-document.write(favoriteFood);
+  {name: 'Salamence',
+  type: ['Dragon','Flying'],
+  height: '1.5 m',
+  weight: '102 kg',
+  pokeDexNum: '373'},
+
+  {name:'Rayquaza',
+  type: ['Dragon','Flying'],
+  height: '7 m',
+  weight:'206.5 kg',
+  pokeDexNum:'384'}
+];
