6f64d2e22ef34f07dfe5219cc1e6e954a82a4844
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-01-21 23:47:03 -0500

    adjusted height and weight values
    previously written as strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 var pokeRepository = [
   {name: 'Dragonite',
   type: ['Dragon','Flying'],
-  height: '2.2 m',
-  weight: '210 kg',
+  height: 2.2,
+  weight: 210,
   pokeDexNum: '149'},
 
   {name: 'Salamence',
   type: ['Dragon','Flying'],
-  height: '1.5 m',
-  weight: '102 kg',
+  height: 1.5,
+  weight: 102,
   pokeDexNum: '373'},
 
   {name:'Rayquaza',
   type: ['Dragon','Flying'],
-  height: '7 m',
-  weight:'206.5 kg',
+  height: 7,
+  weight:206.5,
   pokeDexNum:'384'}
 ];
