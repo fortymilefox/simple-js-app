1a0b0e4b8e0e5c7fd30aa1ce6d0659e49fdecb71
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-01-27 01:08:57 -0500

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,26 @@ var repository = [
     name: 'Dragonite',
     height: 2.2,
     weight: 210,
-    type: ['dragon', 'flying']
+    type: ['Dragon ', ' Flying']
   },
   {
     name: 'Salamence',
     height: 1.5,
     weight: 102,
-    type: ['dragon', 'flying']
+    type: ['Dragon ', ' Flying']
   },
   {
     name: 'Rayquaza',
     height: 7,
     weight: 206.5,
-    type: ['dragon', 'flying']
+    type: ['Dragon ', ' Flying']
   }
 ];
 
 
 for(var i = 0; i < repository.length; i++){
-  document.write('<br>' + 'PokéName: ' + repository[i].name + ' Height: ' + repository[i].height + 'Type: ' + repository[i].type);
-  if (repository.height > 5.5){
+  document.write('<br>' + 'PokéName: ' + repository[i].name + '   Height: ' + repository[i].height + '  Type: ' + repository[i].type);
+  if (repository.height[i] > 5.5){
     document.write(' (--WOW! That\'s a big Pokémon!)')
   }
 }
