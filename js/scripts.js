3230374dac7f46fcf6219901b9b9b6da866ace66
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-01-26 20:13:03 -0500

    replaced repository array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-var pokeRepository = [
-  {name: 'Dragonite',
-  type: ['Dragon','Flying'],
-  height: 2.2,
-  weight: 210,
-  pokeDexNum: '149'},
-
-  {name: 'Salamence',
-  type: ['Dragon','Flying'],
-  height: 1.5,
-  weight: 102,
-  pokeDexNum: '373'},
-
-  {name:'Rayquaza',
-  type: ['Dragon','Flying'],
-  height: 7,
-  weight:206.5,
-  pokeDexNum:'384'}
+var repository = [
+  {
+    name: 'Dragonite',
+    height: 2.2,
+    weight: 210,
+    types: ['dragon', 'flying']
+  },
+  {
+    name: 'Salamence',
+    height: 1.5,
+    weight: 102,
+    types: ['dragon', 'flying']
+  },
+  {
+    name: 'Rayquaza',
+    height: 7,
+    weight: 206.5,
+    types: ['dragon', 'flying']
+  }
 ];
