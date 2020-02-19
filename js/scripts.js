dd7f0441edfbe98817d809f4e8287393f4bc0345
fortymilefox <59665281+fortymilefox@users.noreply.github.com>
2020-02-19 16:53:43 -0500

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,31 @@
 var pokeRepository = (function(){
   var repository = [
     {
-      name: 'Dragonite',
-      height: 2.2,
-      weight: 210,
-      type: ['Dragon ', ' Flying']
-    },
-    {
-      name: 'Salamence',
-      height: 1.5,
-      weight: 102,
-      type: ['Dragon ', ' Flying']
-    },
-    {
-      name: 'Rayquaza',
-      height: 7,
-      weight: 206.5,
-      type: ['Dragon ', ' Flying']
-    }
-  ];
+    name: 'Dragonite',
+    height: 2.2,
+    weight: 210,
+    type: ['Dragon ', ' Flying']
+  },
+  {
+    name: 'Salamence',
+    height: 1.5,
+    weight: 102,
+    type: ['Dragon ', ' Flying']
+  },
+  {
+    name: 'Rayquaza',
+    height: 7,
+    weight: 206.5,
+    type: ['Dragon ', ' Flying']
+  },
+
+  {
+    name: 'Golem',
+    height: 1.4,
+    type:['Rock', 'Ground']
+  },
+]
+
 
 function add(pokemon) {
   repository.push(pokemon);
@@ -29,32 +36,32 @@ function getAll() {
   return repository;
 }
 
+function addListItem(pokemon){
+  //list item & button for each Pokémon
+  var listItem = document.createElement('li');
+  var button = document.createElement('button');
+  $pokeList.appendChild(listItem);
+  //innerText
+  button.innerText = pokemon.name;
+  //class for button
+  button.classList.add('list-button');
+  //append button to list items
+  listItem.appendChild(button);
+
+}
 return {
   add: add,
-  getAll : getAll
+  getAll : getAll,
+  addListItem: addListItem
 };
+
 })();
 
 
-
-pokeRepository.add({name: 'Golem', height: 1.4, type:['Rock', 'Ground']});
-console.log(pokeRepository.getAll());
-
 //Node select for Pokémon list
-var pokeRepository = document.querySelector('.poke-list');
+var $pokeList = document.querySelector('.poke-list');
 
 //forEach loop
-pokeRepository.getAll().forEach(function(detail){
-  //list item & button for each Pokémon
-  var $listItem = document.createElement('li');
-  $pokeList.append($listItem);
-  var $pokeButton = document.createElement('button');
-  //innerText
-  $pokeButton.innerText = pokemon.name;
-  //class for button
-  $pokeButton.classList.add('list-button');
-  //append button to list items
-  $listItem.appendChild($pokeButton);
-  }
-
-});
+pokeRepository.getAll().forEach(function(newItem){
+  pokeRepository.addListItem(newItem);
+})
